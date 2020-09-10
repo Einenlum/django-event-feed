e33e74350216e5aa65d01dfada9d8c93a3f4873a
Einenlum <yann.rabiller@gmail.com>
2020-09-10 15:14:56 +0200

    Add redoc documentation

--- eventfeed/urls.py
+++ eventfeed/urls.py
@@ -15,5 +15,22 @@ Including another URLconf
 """
 from django.contrib import admin
 from django.urls import path, include
+from drf_yasg import openapi
+from drf_yasg.views import get_schema_view
+from rest_framework import permissions
 
-urlpatterns = [path("admin/", admin.site.urls), path("", include("api.urls"))]
+schema_view = get_schema_view(
+    openapi.Info(
+        title="Event Feed API",
+        default_version="v1",
+        description="Some toy project API to play with DRF",
+    ),
+    public=True,
+    permission_classes=(permissions.AllowAny,),
+)
+
+urlpatterns = [
+    path("admin/", admin.site.urls),
+    path("doc/", schema_view.with_ui("redoc", cache_timeout=0)),
+    path("", include("api.urls")),
+]
