6c6dc32bee99963bee50b5c7d148b3da9b876073
Einenlum <yann.rabiller@gmail.com>
2020-09-11 09:25:37 +0200

    Reorganize urls

--- api/urls.py
+++ api/urls.py
@@ -1,9 +1,22 @@
 from django.urls import path
+from drf_yasg.views import get_schema_view
+from drf_yasg import openapi
+from rest_framework import permissions
 from .views.events import EventCollectionAPIView, EventResourceAPIView, attend_an_event
 from .views.authentication import ObtainAuthToken
 
+schema_view = get_schema_view(
+    openapi.Info(
+        title="Event Feed API",
+        default_version="v1",
+        description="Some toy project API to play with DRF",
+    ),
+    public=True,
+    permission_classes=(permissions.AllowAny,),
+)
 
 urlpatterns = [
+    path("doc/", schema_view.with_ui("swagger", cache_timeout=0)),
     path("auth/authenticate/", ObtainAuthToken.as_view()),
     path(
         r"events/",
