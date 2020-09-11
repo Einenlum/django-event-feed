8643388d84becb10731f94d36a161463046a61eb
Einenlum <yann.rabiller@gmail.com>
2020-09-11 17:47:25 +0200

    Add static root to serve assets

--- eventfeed/settings.py
+++ eventfeed/settings.py
@@ -32,6 +32,7 @@ SECRET_KEY = env("SECRET_KEY")
 DEBUG = env("DEBUG")
 
 ALLOWED_HOSTS = [
+    "localhost",
     "django-event-feed.herokuapp.com",
 ]
 
@@ -135,6 +136,7 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/3.0/howto/static-files/
 
 STATIC_URL = "/static/"
+STATIC_ROOT = f"{BASE_DIR}/static/"
 
 REST_FRAMEWORK = {
     "DEFAULT_AUTHENTICATION_CLASSES": [
