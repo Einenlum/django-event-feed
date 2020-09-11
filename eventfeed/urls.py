8dc987fcebdd4929741e9e37e7ee05ebf81a7f7d
Einenlum <yann.rabiller@gmail.com>
2020-09-11 09:20:20 +0200

    Use swagger instead of redoc
    swagger allows to directly test out the API. Not sure why redoc does not
    allow this.

--- eventfeed/urls.py
+++ eventfeed/urls.py
@@ -31,6 +31,6 @@ schema_view = get_schema_view(
 
 urlpatterns = [
     path("admin/", admin.site.urls),
-    path("doc/", schema_view.with_ui("redoc", cache_timeout=0)),
+    path("doc/", schema_view.with_ui("swagger", cache_timeout=0)),
     path("", include("api.urls")),
 ]
